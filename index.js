41c0985f2a7eba90f8aa7c2e5f8a9c9d1b14362b
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-01-09 00:03:14 +0300

    commit all

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// for loop and while loop
+
+function writeCards(array1, event) {
+  const array2 = [];
+  for (let i = 0; i < array1.length; i++) {
+    array2.push(`Thank you, ${array1[i]}, for the wonderful ${event} gift!`);
+  }
+  return array2;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "Suprise"));
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+
+countDown(10);
